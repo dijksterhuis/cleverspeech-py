FIX: Silence examples would never optimise. gradients were always zero with zero valued (silence) original examples. fixed by making randome warm up a mandatory procedure step.

graph/PerturbationSubGraphs.py
@@ -103,62 +103,8 @@ class AbstractPerturbationSubGraph(ABC):
         return self.opt_vars
 
     @abstractmethod
-    def deltas_apply(self, deltas, func):
+    def deltas_apply(self, sess, func):
         pass
-
-    def apply_perturbation_rounding(self, sess):
-
-        """
-        For each perturbation sample (delta_n) find the closest integer
-        less than the current float value.
-
-        This helps the attacks work against the deepspeech native client api
-        which only accepts tf.int16 type inputs. Although it doesn't work 100%
-        of the time so use `bin/classify.py` to get the true success rate.
-
-        We could do this after running optimisation, but doing things during
-        attacks seems to help find a 16 bit int solution... At least that's my
-        excuse.
-
-        N.B. Reassign perturbations that were bounded by the hard constraint
-        => raw_delta samples values can be much larger than the final_delta
-        sample values.
-
-        :param sess: a tensorflow session object
-        :return: None
-        """
-        def rounding_func(delta):
-            signs = np.sign(delta)
-            abs_floor = np.floor(np.abs(delta))
-            return signs * abs_floor
-
-        deltas = sess.run(self.final_deltas)
-        assign_op = self.deltas_apply(deltas, rounding_func)
-        sess.run(assign_op)
-
-    def apply_perturbation_randomisation(self, sess, bit_depth_percent=0.5):
-
-        """
-        Randomise each perturbation sample (delta_n) using a random uniform
-        distribution, with max and min values based on a percentage of the
-        perturbation bit depth.
-
-        :param sess: a tensorflow session object
-        :param bit_depth_percent: base the random uniform dist. on our bit depth
-        :return: None
-        """
-        def random_uniform_func(delta):
-
-            rand_uni = np.random.uniform(
-                -bit_depth_percent * self.__bit_depth,
-                bit_depth_percent * self.__bit_depth,
-                delta.shape
-            )
-            return delta + rand_uni
-
-        deltas = sess.run(self.final_deltas)
-        assign_op = self.deltas_apply(deltas, random_uniform_func)
-        sess.run(assign_op)
 
 
 class Independent(AbstractPerturbationSubGraph):
@@ -203,9 +149,10 @@ class Independent(AbstractPerturbationSubGraph):
 
         return tf.stack(self.raw_deltas, axis=0)
 
-    def deltas_apply(self, deltas, func):
+    def deltas_apply(self, sess, func):
 
         assign_ops = []
+        deltas = sess.run(self.final_deltas)
 
         for idx, delta in enumerate(deltas):
 
@@ -215,7 +162,7 @@ class Independent(AbstractPerturbationSubGraph):
                 self.raw_deltas[idx].assign(new_delta)
             )
 
-        return assign_ops
+        sess.run(assign_ops)
 
 
 class Batch(AbstractPerturbationSubGraph):
@@ -252,9 +199,10 @@ class Batch(AbstractPerturbationSubGraph):
         self.opt_vars = [self.raw_deltas]
         return self.raw_deltas
 
-    def deltas_apply(self, deltas, func):
+    def deltas_apply(self, sess, func):
 
         new_deltas = []
+        deltas = sess.run(self.final_deltas)
 
         for idx, delta in enumerate(deltas):
 
@@ -264,7 +212,7 @@ class Batch(AbstractPerturbationSubGraph):
         new_deltas = np.asarray(new_deltas)
         assign_op = [self.raw_deltas.assign(new_deltas)]
 
-        return assign_op
+        sess.run(assign_op)
 
 
 class Synthesis:

graph/Procedures.py
@@ -8,7 +8,7 @@ actually doing an attack.
 --------------------------------------------------------------------------------
 """
 
-
+import numpy as np
 import tensorflow as tf
 from abc import ABC, abstractmethod
 
@@ -81,7 +81,18 @@ class AbstractProcedure(ABC):
 
         For example, you could start with randomised perturbations.
         """
-        pass
+        def random_uniform_func(delta):
+
+            rand_uni = np.random.uniform(
+                0.002 * self.attack.bit_depth * -1,
+                0.002 * self.attack.bit_depth,
+                delta.shape
+            )
+            return delta + rand_uni
+
+        self.attack.delta_graph.deltas_apply(
+            self.attack.sess, random_uniform_func
+        )
 
     def post_optimisation_hook(self):
         """
@@ -90,7 +101,15 @@ class AbstractProcedure(ABC):
         N.B. This method is not abstract as it is *not* required to run an
         attack, but feel free to override it.
         """
-        pass
+
+        def rounding_func(delta):
+            signs = np.sign(delta)
+            abs_floor = np.floor(np.abs(delta))
+            return signs * abs_floor
+
+        self.attack.delta_graph.deltas_apply(
+            self.attack.sess, rounding_func
+        )
 
     @abstractmethod
     def check_for_successful_examples(self):
@@ -147,33 +166,6 @@ class AbstractProcedure(ABC):
             # Do the actual optimisation
             a.optimiser.optimise(a.feeds.attack)
             self.current_step += 1
-
-
-class RandomUniformWarmUp(AbstractProcedure):
-    def do_warm_up(self):
-        """
-        Should anything else be done before we start?
-
-        N.B. This method is not abstract as it is *not* required to run an
-        attack, but feel free to override it.
-
-        For example, you could start with randomised perturbations.
-        """
-        self.attack.delta_graph.apply_perturbation_randomisation(
-            self.attack.sess, bit_depth_percent=0.5
-        )
-
-
-class IntegerRoundingHook(AbstractProcedure):
-    def post_optimisation_hook(self):
-        """
-        Apply integer level rounding to the perturbations after a certain number
-        of optimisation steps have completed.
-        """
-
-        self.attack.delta_graph.apply_perturbation_rounding(
-            self.attack.sess
-        )
 
 
 class Unbounded(AbstractProcedure):
@@ -368,7 +360,7 @@ class UpdateOnLoss(UpdateOnSuccess):
     Perform updates when loss reaches a specified threshold.
     """
 
-    def __init__(self, attack, *args, loss_lower_bound=10.0, **kwargs):
+    def __init__(self, attack, *args, loss_lower_bound=0.1, **kwargs):
 
         super().__init__(attack, *args, **kwargs)
         self.init_optimiser_variables()
@@ -431,7 +423,15 @@ class UpdateOnDeepSpeechProbs(UpdateOnSuccess):
                 yield False
 
 
-class StandardProcedure(UpdateOnDecoding, IntegerRoundingHook):
+class StandardProcedure(UpdateOnDecoding):
+    pass
+
+
+class SimpleEvasion(UpdateOnDecoding):
+    pass
+
+
+class HighConfidenceEvasion(UpdateOnLoss):
     pass
 
 
@@ -482,7 +482,11 @@ class CTCAlignUpdateOnDecode(UpdateOnDecoding, CTCAlignMixIn):
     pass
 
 
-class StandardCTCAlignProcedure(CTCAlignUpdateOnDecode, IntegerRoundingHook):
+class StandardCTCAlignProcedure(CTCAlignUpdateOnDecode):
+    pass
+
+
+class SimpleCTCAlignEvasion(CTCAlignUpdateOnDecode):
     pass
 
 
@@ -504,6 +508,10 @@ class CTCAlignUpdateOnLoss(UpdateOnLoss, CTCAlignMixIn):
 
     Perform updates when loss reaches a specified threshold.
     """
+    pass
+
+
+class HighConfidenceCTCAlignEvasion(CTCAlignUpdateOnLoss):
     pass
 
 