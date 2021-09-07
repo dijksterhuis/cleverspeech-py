681379aef4bc29e5edc9e02a3e72d1a969e01866
dijksterhuis <mrobeson@dundee.ac.uk>
2021-09-07 16:53:47 +0100

    ADD: ETL+script to do "two stage" attack for **alignment** targets

--- data/ingress/mcv_v1.py
+++ data/ingress/mcv_v1.py
@@ -255,6 +255,141 @@ class Targets(IterableETL):
         return indices
 
 
+class SecondStageTargets(IterableETL):
+
+    def __init__(self, indir):
+
+        self.pool = list(
+            filter(
+                lambda x: ".json" in x and "sample" in x,
+                l_map(lambda i: os.path.join(indir, i), os.listdir(indir))
+           )
+        )
+        self.indir = indir
+
+    def __next__(self, *args):
+        return self.create_batch(*args)
+
+    def create_batch(self, batch_size, trues_batch, audios_batch):
+
+        jsonified_basenames = l_map(
+            lambda x: x.replace(".wav", ".json"), audios_batch["basenames"]
+        )
+
+        ordered_target_file_paths = l_map(
+            lambda x: self.pool.pop(
+                self.pool.index(os.path.join(self.indir, x))
+            ),
+            jsonified_basenames
+        )
+
+        def open_json(fpath):
+            with open(fpath, 'r') as f:
+                data = json.load(f)[0]
+            return data
+
+        batched_json_data = l_map(
+            lambda x: open_json(x), ordered_target_file_paths
+        )
+
+        target_phrases = l_map(
+            lambda x: x["phrases"][0], batched_json_data
+        )
+        row_ids = l_map(
+            lambda x: x["row_ids"][0], batched_json_data
+        )
+        tokens = batched_json_data[0]["tokens"][0]
+        lengths = l_map(
+            lambda x: int(x["lengths"][0]), batched_json_data
+        )
+        original_indices = l_map(
+            lambda x: x["original_indices"], batched_json_data
+        )
+        indices = l_map(
+            lambda x: np.argmax(x["softmax_logits"], axis=-1),
+            batched_json_data
+        )
+
+        def validate(argmaxed, original):
+            merged_argmax = []
+            prev = None
+
+            for entry in argmaxed:
+
+                if prev is None and entry != 28:
+                    merged_argmax.append(entry)
+
+                elif prev is None:
+                    merged_argmax.append(entry)
+
+                elif prev == entry:
+                    pass
+
+                else:
+                    merged_argmax.append(entry)
+
+                prev = entry
+
+            reduced_argmax = list(filter(lambda x: x != 28, merged_argmax))
+
+            return all(x == y for x, y in zip(reduced_argmax, original))
+
+        validate_merge_reduce = l_map(
+            lambda x: validate(*x),
+            zip(indices, original_indices)
+        )
+        try:
+            assert all(x is True for x in validate_merge_reduce)
+
+        except AssertionError as e:
+
+            s = "\033[1;31mERROR:\033[1;0m "
+            s += "Not all target alignments match the intended target phrase!"
+            s += "\n\nCheck your input data located in {}".format(self.indir)
+            s += "\n==> Make sure that argmax(softmax, axis=-1) "
+            s += "matches the target phrases"
+
+            errors = l_map(
+                lambda x: (x[0], x[1]),
+                enumerate(validate_merge_reduce)
+            )
+
+            filtered_errors = l_map(
+                lambda x: x[0],
+                list(filter(lambda x: x[1] is False, errors))
+            )
+
+            error_basenames = l_map(
+                lambda i: jsonified_basenames[i],
+                filtered_errors
+            )
+
+            s += "\n\nThe following files are causing errors:\n"
+            s += "\n".join(error_basenames)
+
+            log(s)
+            raise
+
+        maxlen = audios_batch["max_feats"]
+
+        padded_indices = np_arr(
+            l_map(
+                lambda x: np.concatenate([x, np.zeros(maxlen - len(x))]),
+                indices
+            ),
+            np.int32
+        )
+
+        return {
+            "tokens": tokens,
+            "phrases": target_phrases,
+            "row_ids": row_ids,
+            "indices": padded_indices,
+            "original_indices": original_indices,  # may modify for alignments
+            "lengths": lengths,
+        }
+
+
 def create_true_batch(audio_batch, tokens=TOKENS):
     metadata_fps = l_map(
         lambda fp: fp.replace(".wav", ".json"), audio_batch["file_paths"]
---
+++ scripts/two_stage_mod_attacks.py
@@ -0,0 +1,300 @@
+#!/usr/bin/env python3
+import os
+from cleverspeech import data
+from cleverspeech import graph
+from cleverspeech import models
+
+from cleverspeech.config.arguments_parser import args
+from cleverspeech.runtime.Execution import default_manager
+from cleverspeech.utils.Utils import log
+
+
+def only_box_constraint_graph(sess, batch, settings):
+
+    attack = graph.GraphConstructor.Constructor(
+        sess, batch, settings
+    )
+    attack.add_placeholders(
+        graph.Placeholders.Placeholders
+    )
+    attack.add_perturbation_subgraph(
+        graph.Perturbations.BoxConstraintOnly,
+        random_scale=settings["delta_randomiser"]
+    )
+    attack.add_victim(
+        models.DeepSpeech.Model,
+        decoder=settings["decoder"],
+        beam_width=settings["beam_width"]
+    )
+    if settings["loss"] in KAPPA_LOSSES:
+        attack.add_loss(
+            graph.losses.adversarial.AlignmentBased.GREEDY[settings["loss"]],
+            use_softmax=settings["use_softmax"],
+            k=settings["kappa"]
+        )
+    else:
+        attack.add_loss(
+            graph.losses.adversarial.AlignmentBased.GREEDY[settings["loss"]],
+            use_softmax=settings["use_softmax"],
+        )
+    attack.create_loss_fn()
+    attack.add_optimiser(
+        graph.Optimisers.AdamIndependentOptimiser,
+        learning_rate=settings["learning_rate"]
+    )
+    attack.add_procedure(
+        graph.Procedures.SuccessOnDecoding,
+        steps=settings["nsteps"],
+        update_step=settings["decode_step"]
+    )
+
+    return attack
+
+
+def clipped_gradient_descent_graph(sess, batch, settings):
+
+    attack = graph.GraphConstructor.Constructor(
+        sess, batch, settings
+    )
+    attack.add_placeholders(
+        graph.Placeholders.Placeholders
+    )
+    attack.add_perturbation_subgraph(
+        graph.Perturbations.ClippedGradientDescent,
+        random_scale=settings["delta_randomiser"],
+        constraint_cls=graph.Constraints.L2,
+        r_constant=settings["rescale"],
+        update_method=settings["constraint_update"],
+    )
+    attack.add_victim(
+        models.DeepSpeech.Model,
+        decoder=settings["decoder"],
+        beam_width=settings["beam_width"]
+    )
+
+    if settings["loss"] in KAPPA_LOSSES:
+        attack.add_loss(
+            graph.losses.adversarial.AlignmentBased.GREEDY[settings["loss"]],
+            use_softmax=settings["use_softmax"],
+            k=settings["kappa"],
+            weight_settings=(1.0e3, 0.5),
+            updateable=True,
+        )
+    else:
+        attack.add_loss(
+            graph.losses.adversarial.AlignmentBased.GREEDY[settings["loss"]],
+            use_softmax=settings["use_softmax"],
+            weight_settings=(1.0e3, 0.5),
+            updateable=True,
+        )
+    attack.create_loss_fn()
+    attack.add_optimiser(
+        graph.Optimisers.AdamIndependentOptimiser,
+        learning_rate=settings["learning_rate"]
+    )
+    attack.add_procedure(
+        graph.Procedures.SuccessOnDecoding,
+        steps=settings["nsteps"],
+        update_step=settings["decode_step"]
+    )
+
+    return attack
+
+
+def clipped_linf_with_l2_loss(sess, batch, settings):
+
+    attack = graph.GraphConstructor.Constructor(
+        sess, batch, settings
+    )
+    attack.add_placeholders(
+        graph.Placeholders.Placeholders
+    )
+    attack.add_perturbation_subgraph(
+        graph.Perturbations.ClippedGradientDescent,
+        random_scale=settings["delta_randomiser"],
+        constraint_cls=graph.Constraints.Linf,
+        r_constant=settings["rescale"],
+        update_method=settings["constraint_update"],
+    )
+    attack.add_victim(
+        models.DeepSpeech.Model,
+        decoder=settings["decoder"],
+        beam_width=settings["beam_width"]
+    )
+
+    if settings["loss"] in KAPPA_LOSSES:
+        attack.add_loss(
+            graph.losses.adversarial.AlignmentBased.GREEDY[settings["loss"]],
+            use_softmax=settings["use_softmax"],
+            k=settings["kappa"],
+            weight_settings=(1.0e3, 0.5),
+            updateable=True,
+        )
+    else:
+        attack.add_loss(
+            graph.losses.adversarial.AlignmentBased.GREEDY[settings["loss"]],
+            use_softmax=settings["use_softmax"],
+            weight_settings=(1.0e3, 0.5),
+            updateable=True,
+        )
+    attack.add_loss(
+        graph.losses.Distance.L2CarliniLoss,
+        weight_settings=(1.0e-3, 1),
+        updateable=True,
+    )
+    attack.create_loss_fn()
+    attack.add_optimiser(
+        graph.Optimisers.AdamIndependentOptimiser,
+        learning_rate=settings["learning_rate"]
+    )
+    attack.add_procedure(
+        graph.Procedures.SuccessOnDecoding,
+        steps=settings["nsteps"],
+        update_step=settings["decode_step"]
+    )
+
+    return attack
+
+
+def clipped_l2_with_linf_loss(sess, batch, settings):
+
+    attack = graph.GraphConstructor.Constructor(
+        sess, batch, settings
+    )
+    attack.add_placeholders(
+        graph.Placeholders.Placeholders
+    )
+    attack.add_perturbation_subgraph(
+        graph.Perturbations.ClippedGradientDescent,
+        random_scale=settings["delta_randomiser"],
+        constraint_cls=graph.Constraints.L2,
+        r_constant=settings["rescale"],
+        update_method=settings["constraint_update"],
+    )
+    attack.add_victim(
+        models.DeepSpeech.Model,
+        decoder=settings["decoder"],
+        beam_width=settings["beam_width"]
+    )
+
+    if settings["loss"] in KAPPA_LOSSES:
+        attack.add_loss(
+            graph.losses.adversarial.AlignmentBased.GREEDY[settings["loss"]],
+            use_softmax=settings["use_softmax"],
+            k=settings["kappa"],
+            weight_settings=(1.0e3, 0.5),
+            updateable=True,
+        )
+    else:
+        attack.add_loss(
+            graph.losses.adversarial.AlignmentBased.GREEDY[settings["loss"]],
+            use_softmax=settings["use_softmax"],
+            weight_settings=(1.0e3, 0.5),
+            updateable=True,
+        )
+    attack.add_loss(
+        graph.losses.Distance.LinfLoss,
+        weight_settings=(1.0e-3, 1),
+        updateable=False,
+    )
+    attack.create_loss_fn()
+    attack.add_optimiser(
+        graph.Optimisers.AdamIndependentOptimiser,
+        learning_rate=settings["learning_rate"]
+    )
+    attack.add_procedure(
+        graph.Procedures.SuccessOnDecoding,
+        steps=settings["nsteps"],
+        update_step=settings["decode_step"]
+    )
+
+    return attack
+
+
+def attack_run(master_settings):
+    """
+    Use Carlini & Wagner's improved loss function form the original audio paper,
+    but reintroduce kappa from the image attack as we're looking to perform
+    targeted maximum-confidence evasion attacks --- i.e. not just find minimum
+    perturbations.
+
+    :param master_settings: a dictionary of arguments to run the attack, as
+    defined by command line arguments. Will override the settings dictionary
+    defined below.
+
+    :return: None
+    """
+
+    master_settings["use_softmax"] = bool(master_settings["use_softmax"])
+
+    if master_settings["loss"] in KAPPA_LOSSES:
+        assert master_settings["kappa"] is not None
+        if master_settings["use_softmax"] is True:
+            assert 0 <= master_settings["kappa"] < 1
+        else:
+            assert master_settings["kappa"] >= 0
+
+    attack_type = master_settings["attack_graph"]
+    align = master_settings["align"]
+    loss = master_settings["loss"]
+    decoder = master_settings["decoder"]
+    kappa = master_settings["kappa"]
+    outdir = master_settings["outdir"]
+
+    outdir = os.path.join(outdir, attack_type)
+    outdir = os.path.join(outdir, "{}/".format(loss))
+    outdir = os.path.join(outdir, "{}/".format(align))
+    outdir = os.path.join(outdir, "{}/".format(decoder))
+    outdir = os.path.join(outdir, "k{}/".format(kappa))
+
+    master_settings["outdir"] = outdir
+    master_settings["attack type"] = attack_type
+
+    audios = data.ingress.mcv_v1.Audios(
+        master_settings["audio_indir"],
+        master_settings["max_examples"],
+        filter_term=".wav",
+        max_file_size=master_settings["max_audio_file_bytes"]
+    )
+
+    transcriptions = data.ingress.mcv_v1.SecondStageTargets(
+        master_settings["targets_path"],
+    )
+
+    batch_gen = data.ingress.mcv_v1.BatchIterator(
+        master_settings, audios, transcriptions
+    )
+
+    default_manager(
+        master_settings,
+        ATTACK_GRAPHS[master_settings["attack_graph"]],
+        batch_gen,
+    )
+    log("Finished run.")
+
+
+ATTACK_GRAPHS = {
+    "box": only_box_constraint_graph,
+    "cgd": clipped_gradient_descent_graph,
+    "linf_l2": clipped_linf_with_l2_loss,
+    "l2_linf": clipped_l2_with_linf_loss,
+}
+
+KAPPA_LOSSES = ["cw", "weightedmaxmin", "adaptivekappa"]
+
+
+def main():
+    log("", wrap=True)
+
+    extra_args = {
+        "attack_graph": [str, "box", True, ATTACK_GRAPHS.keys()],
+        "loss": [str, None, True, graph.losses.adversarial.AlignmentBased.GREEDY.keys()],
+        "kappa": [float, None, False, None],
+        'use_softmax': [int, 0, False, [0, 1]],
+    }
+
+    args(attack_run, additional_args=extra_args)
+
+
+if __name__ == '__main__':
+    main()
