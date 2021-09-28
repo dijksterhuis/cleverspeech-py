a75b31de3a5d5efbbf60b8c72e41adb856af2260
dijksterhuis <mrobeson@dundee.ac.uk>
2021-09-28 01:54:01 +0100

    Sanify the script names for icaasp

--- scripts/icaasp2022/normalised_and_trimmed.py
+++ scripts/icaasp2022/normalised_and_trimmed.py
@@ -333,6 +333,89 @@ def cgd_cw_patchmid_align_greedy_search_graph(sess, batch, settings):
     return attack
 
 
+
+def cgd_cw_dist_minnergy_align_greedy_search_graph(sess, batch, settings):
+
+    attack = graph.GraphConstructor.Constructor(
+        sess, batch, settings
+    )
+    attack.add_path_search(
+      graph.Paths.MinimumEnergyDistributed
+    )
+    attack.add_placeholders(
+        graph.Placeholders.Placeholders
+    )
+    attack.add_perturbation_subgraph(
+        graph.Perturbations.ClippedGradientDescent,
+        random_scale=settings["delta_randomiser"],
+        constraint_cls=graph.Constraints.L2,
+        r_constant=settings["rescale"],
+        update_method="geom",
+    )
+    attack.add_victim(
+        models.DeepSpeech.Model,
+        decoder="tf_greedy",
+    )
+    attack.add_loss(
+        graph.losses.adversarial.AlignmentBased.CWMaxMin,
+        weight_settings=(1.0e3, 0.5),
+        updateable=True,
+    )
+    attack.create_loss_fn()
+    attack.add_optimiser(
+        graph.Optimisers.AdamIndependentOptimiser,
+        learning_rate=settings["learning_rate"],
+    )
+    attack.add_procedure(
+        graph.Procedures.SuccessOnDecoding,
+        steps=settings["nsteps"],
+        update_step=settings["decode_step"],
+    )
+
+    return attack
+
+
+def cgd_cw_patch_minnergy_align_greedy_search_graph(sess, batch, settings):
+
+    attack = graph.GraphConstructor.Constructor(
+        sess, batch, settings
+    )
+    attack.add_path_search(
+      graph.Paths.MinimumEnergyPatch
+    )
+    attack.add_placeholders(
+        graph.Placeholders.Placeholders
+    )
+    attack.add_perturbation_subgraph(
+        graph.Perturbations.ClippedGradientDescent,
+        random_scale=settings["delta_randomiser"],
+        constraint_cls=graph.Constraints.L2,
+        r_constant=settings["rescale"],
+        update_method="geom",
+    )
+    attack.add_victim(
+        models.DeepSpeech.Model,
+        decoder="tf_greedy",
+    )
+    attack.add_loss(
+        graph.losses.adversarial.AlignmentBased.CWMaxMin,
+        weight_settings=(1.0e3, 0.5),
+        updateable=True,
+    )
+    attack.create_loss_fn()
+    attack.add_optimiser(
+        graph.Optimisers.AdamIndependentOptimiser,
+        learning_rate=settings["learning_rate"],
+    )
+    attack.add_procedure(
+        graph.Procedures.SuccessOnDecoding,
+        steps=settings["nsteps"],
+        update_step=settings["decode_step"],
+    )
+
+    return attack
+
+
 def attack_run(master_settings):
 
     run = master_settings["set"]
@@ -343,7 +426,7 @@ def attack_run(master_settings):
     settings["attack_graph"] = run
     settings["outdir"] = os.path.join(settings["outdir"], run)
 
-    audios = data.ingress.mcv_v1.Audios(
+    audios = data.ingress.mcv_v1.NormalisedAndTrimmedAudios(
         settings["audio_indir"],
         settings["max_examples"],
         filter_term=".wav",
@@ -377,6 +460,8 @@ ATTACK_GRAPHS = {
   "cw-patchmid": cgd_cw_patchmid_align_greedy_search_graph,
   "cw-patchend": cgd_cw_patchend_align_greedy_search_graph,
   "ctc-greedy": cgd_ctc_greedy_search_graph,
+  "minnergy-dist": cgd_cw_dist_minnergy_align_greedy_search_graph,
+  "minnergy-patch": cgd_cw_patch_minnergy_align_greedy_search_graph,
 }
 
 
