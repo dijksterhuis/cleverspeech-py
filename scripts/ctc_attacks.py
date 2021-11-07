bf3aa4e3278d7b7c08850da27518943c4848b9e0
dijksterhuis <mrobeson@dundee.ac.uk>
2021-11-07 16:55:37 +0000

    revert ctc script to use vanilla procedure.

--- scripts/ctc_attacks.py
+++ scripts/ctc_attacks.py
@@ -83,10 +83,10 @@ def clipped_gradient_descent_graph(sess, batch, settings):
         learning_rate=settings["learning_rate"]
     )
     attack.add_procedure(
-        graph.Procedures.SuccessOnDecodingWithRestarts,
+        graph.Procedures.SuccessOnDecoding,
         steps=settings["nsteps"],
         update_step=settings["decode_step"],
-        restart_step=settings["restart_step"],
+        # restart_step=settings["restart_step"],
     )
 
     return attack
