50a65ed805b3f545f557425e7eb90049f46ef794
dijksterhuis <mrobeson@dundee.ac.uk>
2021-11-22 16:44:41 +0000

    Some QOL changes and extensions.

--- graph/constraints/bases.py
+++ graph/constraints/bases.py
@@ -32,9 +32,9 @@ class AbstractSizeConstraint(AbstractBoxConstraint):
         assert type(r_constant) == float or type(r_constant) == np.float32
         assert 0 < r_constant < 1.0
 
-        assert update_method in ["lin", "geom", "log"]
+        assert update_method in ["lin", "geom", "log", "geom_binary"]
 
-        self.__bit_depth = bit_depth
+        self.bit_depth = bit_depth
         self.r_constant = r_constant
 
         self.tf_run = sess.run
@@ -48,12 +48,25 @@ class AbstractSizeConstraint(AbstractBoxConstraint):
             name='qq_masks'
         )
 
+        self.previous = tf.Variable(
+            tf.zeros([batch.size, 1]),
+            trainable=False,
+            validate_shape=True,
+            dtype=tf.float32,
+            name='qq_masks'
+        )
+
         self.initial_taus = np_arr(
             lcomp(self._gen_tau(batch.audios["n_samples"])),
             np.float32
         )
 
-        sess.run(self.bounds.assign(self.initial_taus))
+        sess.run(
+            [
+                self.bounds.assign(self.initial_taus),
+                self.previous.assign(self.initial_taus)
+            ]
+        )
 
     def _gen_tau(self, act_lengths):
         """
@@ -62,7 +75,7 @@ class AbstractSizeConstraint(AbstractBoxConstraint):
         samples).
         """
         for l in act_lengths:
-            yield self.analyse([self.__bit_depth for _ in range(l)])
+            yield self.analyse(np.asarray([self.bit_depth for _ in range(l)]))
 
     @abstractmethod
     def analyse(self, x):
@@ -78,22 +91,47 @@ class AbstractSizeConstraint(AbstractBoxConstraint):
         """
         pass
 
-    def get_new_bound(self, distance):
+    def _tf_clipper(self, current):
+        denom = tf.maximum(self.bounds, tf.expand_dims(current, axis=-1))
+        return self.bounds / denom
+
+    def get_new_bound(self, dist):
         """
         Get a new bound with geometric progression.
         """
-        return distance * self.r_constant
+        return dist * self.r_constant
+
+    def revert_bound(self, distance, previous):
+        """
+        Get a new bound with geometric progression.
+        """
+        diff = max(previous, distance) - min(previous, distance)
+        reverted_bound = distance + diff * 0.5 if diff != 0.0 else previous
+        return reverted_bound
 
     def update(self, deltas, successes):
         """
         Update bounds for all perturbations in a batch, if they've found success
         """
-        current_bounds = self.tf_run(self.bounds)
+        tf_bounds = [self.bounds, self.previous]
+        current_bounds, previous_bounds = self.tf_run(tf_bounds)
         current_distances = self.analyse(deltas)[0]
 
-        z = zip(current_bounds, current_distances, successes)
-        for idx, (b, d, s) in enumerate(z):
+        z = zip(current_bounds, previous_bounds, current_distances, successes)
+        for idx, (b, p, d, s) in enumerate(z):
+
             if s is True:
+                p[0] = b[0]
                 b[0] = self.get_new_bound(d)
 
-        self.tf_run(self.bounds.assign(current_bounds))
+            # if s is False and "_binary" in self.update_method:
+            #     b[0] = self.revert_bound(d, p[0])
+
+        bound_assigns = [
+            self.bounds.assign(current_bounds),
+            self.previous.assign(previous_bounds)
+        ]
+
+        self.tf_run(bound_assigns)
+
+
