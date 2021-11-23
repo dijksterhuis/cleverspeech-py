57fde49c6e72e52d76d17d10eef8e685701e7c0f
dijksterhuis <mrobeson@dundee.ac.uk>
2021-11-23 01:29:11 +0000

    Add a new Logger class to handle multiple types of info

--- utils/Utils.py
+++ utils/Utils.py
@@ -3,7 +3,7 @@ import numpy as np
 from os import path
 from datetime import datetime as time
 from base64 import b64encode
-
+import sys
 
 def dump_b64bytes(z):
     return b64encode(z.dumps()).decode()
@@ -67,6 +67,79 @@ def log(*args, funcs=None, wrap=True, outdir=None, fname="log.txt", stdout=True,
 
     if stdout:
         print(s)
+
+
+class Logger:
+    @staticmethod
+    def _log(
+            *args,
+            funcs=None,
+            wrap=False,
+            outdir=None,
+            fname="log.txt",
+            stdout=True,
+            timings=False,
+            prefix=None,
+            postfix=None,
+            logger_prefix=None,
+            wrapper="-" * 30
+    ):
+
+        s = "" if not logger_prefix else logger_prefix.format(time.now())
+
+        if args:
+            s += "\n".join(args)
+
+        if funcs:
+            if callable(funcs):
+                s += funcs()
+            else:
+                for func in funcs:
+                    s += func()
+
+        if timings and not logger_prefix:
+            s = "{}: ".format(time.now()) + s
+
+        if wrap:
+            s += "\n" + wrapper
+
+        if prefix:
+            s = prefix + s
+
+        if postfix:
+            s += postfix
+
+        if outdir is not None and fname is not None:
+            outfile = path.join(outdir, fname)
+            with open(outfile, "a+") as f:
+                f.write(s + "\n")
+
+        if stdout:
+            print(s)
+
+    def critical(*args, **kwargs):
+        Logger._log(
+            *args,
+            logger_prefix="\033[5;37;41m{}: CRITICAL ===> \033[1;0m ",
+            **kwargs
+        )
+
+    def warn(*args, **kwargs):
+        Logger._log(
+            *args,
+            logger_prefix="\033[1;30;43m{}: WARNING  ===> \033[1;0m ",
+            **kwargs
+        )
+
+    def info(*args, **kwargs):
+        Logger._log(
+            *args,
+            logger_prefix="\033[1;30;47m{}: INFO     ===> \033[1;0m ",
+            **kwargs
+        )
+
+    def log(*args, **kwargs):
+        Logger._log(*args, **kwargs)
 
 
 class Timer:
